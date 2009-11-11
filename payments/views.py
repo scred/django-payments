9d0a3c3f3deb26a58ae0586c3a492d615c9dbc12
Tuomas Toivonen <toivotuo@kasvua.org>
2009-11-11 16:12:53 +0200

    Views for error and cancel returns

--- payments/views.py
+++ payments/views.py
@@ -3,7 +3,7 @@ from payments.processor import PaymentProcessor
 from payments.exceptions import PaymentProcessingError
 
 def success(request, payment_method, payment_code):
-
+    
     from payments.connector import PaymentConnector
 
     payment = PaymentConnector.get_connector().lookup(payment_code)
@@ -14,4 +14,20 @@ def success(request, payment_method, payment_code):
     except PaymentProcessingError, e:
         return pp.error(request, payment, e)
 
-# FATAL: missing error() and cancel() views completely!
+def error(request, payment_method, payment_code):
+        
+    from payments.connector import PaymentConnector
+
+    payment = PaymentConnector.get_connector().lookup(payment_code)
+    pp = PaymentProcessor.get_processor(payment_method)
+
+    return pp.error(request, payment)
+
+def cancel(request, payment_method, payment_code):
+        
+    from payments.connector import PaymentConnector
+
+    payment = PaymentConnector.get_connector().lookup(payment_code)
+    pp = PaymentProcessor.get_processor(payment_method)
+
+    return pp.cancel(request, payment)
