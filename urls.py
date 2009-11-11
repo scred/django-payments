e97504e5ee6e763adb88bceddd064f1de4607ea5
Tuomas Toivonen <toivotuo@kasvua.org>
2009-11-11 15:52:15 +0200

    Cleaned the example application's URLs

--- urls.py
+++ urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
-urlpatterns = patterns('example.views',
-    (r'^hello/$', 'hello'),
-    (r'^checkout/$', 'checkout'),
-    (r'^query/$', 'query'),
-)
+urlpatterns = patterns('')
 
-urlpatterns += patterns('',
-                        (r'^payment/', include('payments.urls')))
+urlpatterns += patterns(
+    '',
+    (r'^payment/', include('payments.urls')))
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'example.views',
+        (r'^hello/$', 'hello'),
+        (r'^checkout/$', 'checkout'),
+        (r'^query/$', 'query'),
+    )
